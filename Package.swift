c9c14fe0299e5995511f737657b59e640e8a6c13
developers-mylermedia <63100094+developers-mylermedia@users.noreply.github.com>
2022-11-07 10:24:09 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
   platforms: [.macOS(.v10_13)],
   products: [
     .plugin(name: "FormatSwift", targets: ["FormatSwift"]),
+    .executable(name: "FormatSwift", targets: ["FormatSwift"]),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.3"),
