65e40fe28b590362dbb782558788e42620b29fa7
developers-mylermedia <63100094+developers-mylermedia@users.noreply.github.com>
2023-06-22 14:23:36 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "AirbnbSwift",
+  name: "MylerSwift",
   platforms: [.macOS(.v10_13)],
   products: [
     .plugin(name: "FormatSwift", targets: ["FormatSwift"]),
@@ -16,24 +16,26 @@ let package = Package(
       capability: .command(
         intent: .custom(
           verb: "format",
-          description: "Formats Swift source files according to the Airbnb Swift Style Guide"),
+          description: "Formats Swift source files according to the Myler Swift Style Guide"),
         permissions: [
           .writeToPackageDirectory(reason: "Format Swift source files"),
         ]),
       dependencies: [
-        "AirbnbSwiftFormatTool",
+        "MylerSwiftFormatTool",
         "SwiftFormat",
-        "SwiftLintBinary",
+        "SwiftLint",
+        "SwiftGen",
       ]),
 
     .executableTarget(
-      name: "AirbnbSwiftFormatTool",
+      name: "MylerSwiftFormatTool",
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
       ],
       resources: [
-        .process("airbnb.swiftformat"),
+        .process("default.swiftformat"),
         .process("swiftlint.yml"),
+        .process("swiftgen.yml"),
       ]),
 
     .binaryTarget(
@@ -42,9 +44,15 @@ let package = Package(
       checksum: "544892fc9a6464d4bb5e0fc08f74c9b11a0937d73f7af64eafd9ada3b8a2aa7c"),
 
     .binaryTarget(
-      name: "SwiftLintBinary",
+      name: "SwiftLint",
       url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
       checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"),
+    
+    .binaryTarget(
+        name: "SwiftGen",
+        url: "https://github.com/SwiftGen/SwiftGen/releases/download/6.6.2/swiftgen.artifactbundle.zip",
+        checksum: "729d5f2d0c18cc407f82bb416827fb6897bc2eb422f6a776492fa28551f18820"),
+    ]
   ])
 
 // Emit an error on Linux, so Swift Package Manager's platform support detection doesn't say this package supports Linux
